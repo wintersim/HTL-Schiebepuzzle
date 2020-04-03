60f71de6c8c02a5921134fc707b77d795d414299
Simon Wintereder <simon.wintereder@gmx.at>
2020-04-03 15:55:44 +0200

    StartMenü

--- app/src/main/java/cc/catgasm/HTLWSlidingPuzzle/GameActivity.java
+++ app/src/main/java/cc/catgasm/HTLWSlidingPuzzle/GameActivity.java
@@ -4,17 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class GameActivity extends AppCompatActivity {
+
+    private ArrayList<ImageCell> cells = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        TextView tv = findViewById(R.id.wholePicture);
+
         Intent intent = getIntent();
-        tv.setText(String.format("%s%s", tv.getText(), intent.getStringExtra(MainActivity.GAME_SIZE_MESSAGE)));
+        int gridSize = intent.getIntExtra(MainActivity.GAME_SIZE_MESSAGE,3);
+
+        createCells();
+        GridView gridView = findViewById(R.id.gridview);
+        AdapterSub adapterSub = new AdapterSub(this, cells);
+        gridView.setAdapter(adapterSub);
+        gridView.setNumColumns(gridSize);
+    }
+
+    private void createCells() {
+        String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight"};
+        for (int i = 1; i < 9; i++) {
+            cells.add(new ImageCell(("./drawable/" + numbers[i - 1] + ".png"), numbers[i - 1], i));
+        }
+        cells.add(new ImageCell("./drawable/placeholder.png", "placeholder", 8));
+        System.out.println("Cells added.");
     }
 }
--- app/src/main/java/cc/catgasm/HTLWSlidingPuzzle/MainActivity.java
+++ app/src/main/java/cc/catgasm/HTLWSlidingPuzzle/MainActivity.java
@@ -6,46 +6,24 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
-import android.widget.GridView;
-
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    //TODO: Anfangen lol
-
     public static final String GAME_SIZE_MESSAGE = "cc.catgasm.HTLWSlidingPuzzle.GAME_SIZE_MESSAGE";
-
-    private ArrayList<ImageCell> cells = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        createCells();
-        GridView gridView = (GridView) findViewById(R.id.gridview);
-        AdapterSub adapterSub = new AdapterSub(this, cells);
-        gridView.setAdapter(adapterSub);
-        gridView.setNumColumns(3);
     }
-
-
-    private void createCells() {
-        String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight"};
-        for (int i = 1; i < 9; i++) {
-            cells.add(new ImageCell(("./drawable/" + numbers[i - 1] + ".png"), numbers[i - 1], i));
-        }
-        cells.add(new ImageCell("./drawable/placeholder.png", "placeholder", 8));
-        System.out.println("Cells added.");
-    }
-
 
     public void startGame(View view) {
+        /*Gewünschte Größe an GameActivity schicken*/
+
         Intent intent = new Intent(this, GameActivity.class);
-        String sz = "3x3"; //TODO Button oder so in der View
+        int sz = 3; //TODO Button oder so in der View
         intent.putExtra(GAME_SIZE_MESSAGE, sz);
         startActivity(intent);
     }
